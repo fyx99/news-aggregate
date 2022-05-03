f2c2fc9c2e75c0464e4f9a846e3af4f83b5eeadb
fyx99 <bfelix120@gmail.com>
2022-05-03 02:10:20 +0200

    adding rabbit

--- newsaggregate/rss/manager.py
+++ newsaggregate/rss/manager.py
@@ -1,10 +1,11 @@
 import time
 from typing import List
+
 from newsaggregate.db.crud.article import Article, get_random_articles, refresh_article_materialized_views
 from newsaggregate.db.crud.feeds import Feed, get_feeds
 from newsaggregate.db.databaseinstance import DatabaseInterface
 from newsaggregate.db.postgresql import Database
-from newsaggregate.feed.manager import FeedManager
+from newsaggregate.message.rabbit import MessageBroker
 from newsaggregate.rss.rsscrawler import RSSCrawler
 from newsaggregate.rss.htmlcrawler import HTMLCrawler
 from queue import Queue
@@ -15,8 +16,14 @@ logger = get_logger()
 from newsaggregate.storage.s3 import Datalake
 
 
+RSS_CRAWL = "RSS_CRAWL"
+HTML_CRAWL = "HTML_CRAWL"
+FEATURE_EXTRACTION = "FEATURE_EXTRACTION"
+
 class Manager:
     q = Queue()
+    
+    #feature_thread = None
 
     f_job_count = 1
 
@@ -32,27 +39,40 @@ class Manager:
         return Manager.q.empty()
 
     def run(db):
-        for _ in range(10):
+        for _ in range(5):
             worker = threading.Thread(target=Manager.process, args=(db,))
             worker.start()
+            #Manager.feature_thread = worker
+        logger.debug("JOINING")
         Manager.q.join()
         logger.info("ALL JOINED")
 
     def process(db):
+
         while not Manager.empty():
             job = Manager.q.get()
-            RssCrawlManager.process_job(db, job)
+            logger.debug("GET")
+            #logger.info(f"{threading.current_thread().ident} {Manager.feature_thread.ident}")
+            # if job["job_type"] == FEATURE_EXTRACTION and threading.current_thread() is not Manager.feature_thread:
+            #     # workaround to limit feature extraction to single thread - #TODO remove feature extraction from rss
+            #     Manager.q.put(job)
+            #     Manager.q.task_done()
+            #     break
+            try:
+                RssCrawlManager.process_job(db, job)
+            except Exception:
+                # if this is not catched errors will halt thread and cause join to fail
+                logger.info("UNEXPECTED EXCEPTION IN PROCESSING JOB")
+            logger.debug("PROCESSED")
             Manager.f_job_count += 1
             if Manager.f_job_count % 50 == 0:
                 logger.info(f"MANAGER 100 JOBS AT {time.time() - Manager.last_time:.2f} TOTAL {(time.time() - Manager.last_time) / 50:.2f} / IT")
                 Manager.last_time = time.time()
             Manager.q.task_done()
-        logger.info(f"{threading.get_ident()} DONE")
+            logger.debug("TASK DONE")
+        logger.info(f"THREAD DONE")
 
 
-RSS_CRAWL = "RSS_CRAWL"
-HTML_CRAWL = "HTML_CRAWL"
-FEATURE_EXTRACTION = "FEATURE_EXTRACTION"
 
 
 def add_initial_rss_crawl_jobs(db: DatabaseInterface):
@@ -67,13 +87,16 @@ def add_random_status_crawl_jobs(db: DatabaseInterface):
 class RssCrawlManager:
     def main():
         # execute
-        with Database() as db, Datalake() as dl:
-            di = DatabaseInterface(db, dl)
+        with Database() as db, Datalake() as dl, MessageBroker() as rb:
+            di = DatabaseInterface(db, dl, rb)
             HTMLCrawler.get_patterns(di)
             add_initial_rss_crawl_jobs(di)
             #add_random_status_crawl_jobs(di)
             Manager.run(di)
+            
+            logger.info("REFRESH MATERIALIZED VIEWS")
             refresh_article_materialized_views(di)
+            logger.info("REFRESH DONE")
 
     def process_job(db: DatabaseInterface, job):
         job_type: str = job["job_type"]
@@ -84,18 +107,20 @@ class RssCrawlManager:
 
             for article in articles:
                 if article.status != "CRAWL":
-                    if random.random() > 0.2:
+                    if random.random() > 0.05:
                         continue
                 Manager.add_job({"job_type": HTML_CRAWL, "feed": job_feed, "article": article})
 
         elif job_type == HTML_CRAWL:
             HTMLCrawler.run_single(db, job_article)
-            Manager.add_job({"job_type": FEATURE_EXTRACTION, "article": job_article, "feed": job_feed})
+            db.rb.put_task("FEATURE", {"job_type": FEATURE_EXTRACTION, "article": job_article.to_json(), "feed": job_feed.to_json()})
+            logger.info("ADDED JOB TO RABBIT")
+            #Manager.add_job({"job_type": FEATURE_EXTRACTION, "article": job_article, "feed": job_feed})
         
-        elif job_type == FEATURE_EXTRACTION:
-            if job_feed.language != 'EN' and job_feed.language != 'DE':
-                a = 1
-            FeedManager.run_single_article(db, job_article, job_feed)
+        # elif job_type == FEATURE_EXTRACTION:
+        #     if job_feed.language != 'EN' and job_feed.language != 'DE':
+        #         a = 1
+        #     FeedManager.run_single_article(db, job_article, job_feed)
         # extra für jeden logger.info(f"""SIZE {Manager.q.qsize()} RAN JOB {job_type} {job["link"]}""")
 
 
